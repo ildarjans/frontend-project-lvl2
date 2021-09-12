73a85fe6bd6ab9aa84a6c8f0a29b9c53e16ace1d
ildarjan <nobap84@rambler.ru>
2021-09-12 19:42:38 +0300

    add format option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,18 +3,20 @@
 import { Command } from 'commander';
 import parser from '../src/parser.js';
 import getDiff from '../src/getdiff.js';
+import Formats from '../src/formats/formats.js';
 
 const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .option('-f, --format [type]', 'output format')
+  .option('-f, --format [type]', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
-  .action((filepath1, filepath2) => {
+  .action((filepath1, filepath2, options) => {
+    const { format } = options;
     const target = parser(filepath1);
     const source = parser(filepath2);
-    console.log(getDiff(target, source));
+    console.log(Formats[format](getDiff(target, source)));
   });
 
 program.parse(process.argv);
