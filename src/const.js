c1e03efd454d5432e6586848721671040b26c542
ildarjan <nobap84@rambler.ru>
2021-09-12 19:41:30 +0300

    move this const's to separate module

---
+++ src/const.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line import/prefer-default-export
+export const DiffType = {
+  SAME: 'SAME',
+  UPDATED: 'UPDATED',
+  MISSED: 'MISSED',
+  NEW: 'NEW',
+  NESTED: 'NESTED',
+};
