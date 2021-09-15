53132a213259bd624f675c61297e578eacf1b086
ildarjan <nobap84@rambler.ru>
2021-09-15 23:02:09 +0300

    add plain format

---
+++ src/formats/plain.js
@@ -0,0 +1,23 @@
+import _ from 'lodash';
+import { DiffType } from '../const.js';
+
+const getFormattedValue = (value) => {
+  if (_.isObject(value)) { return '[complex value]'; }
+  if (_.isString(value)) { return `'${value}'`; }
+  return value;
+};
+
+const PlainAction = {
+  [DiffType.SAME]: () => [],
+  [DiffType.NEW]: (node, paths) => `Property '${paths.join('.')}' was added with value: ${getFormattedValue(node.newValue)}`,
+  [DiffType.MISSED]: (node, paths) => `Property '${paths.join('.')}' was removed`,
+  [DiffType.UPDATED]: (node, paths) => `Property '${paths.join('.')}' was updated. From ${getFormattedValue(node.oldValue)} to ${getFormattedValue(node.newValue)}`,
+  [DiffType.NESTED]: (node, paths, build) => build(node.children, paths),
+};
+
+export default (diffs) => {
+  const iter = (basicDiffs, paths = []) => basicDiffs
+    .flatMap((diff) => PlainAction[diff.type](diff, [...paths, diff.key], iter));
+
+  return `\n${iter(diffs).join('\n')}\n`;
+};
