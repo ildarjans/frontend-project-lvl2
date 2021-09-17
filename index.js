9f990ddcac127eef60649a7fabfc77d45ffc59b4
ildarjan <nobap84@rambler.ru>
2021-09-18 01:08:29 +0300

    implement main function

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-import getDiff from './src/getdiff.js';
+import parser from './src/parser.js';
+import Formats from './src/formats/index.js';
+import buildDiff from './src/buildDiff.js';
 
-export default getDiff;
+export default (filepath1, filepath2, format) => {
+  const target = parser(filepath1);
+  const source = parser(filepath2);
+  return Formats[format](buildDiff(target, source));
+};
