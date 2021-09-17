9fc008f37bad249322f3d98aeafbf311bd0b6031
ildarjan <nobap84@rambler.ru>
2021-09-18 01:26:21 +0300

    set format default value

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import parser from './src/parser.js';
 import Formats from './src/formats/index.js';
 import buildDiff from './src/buildDiff.js';
 
-export default (filepath1, filepath2, format) => {
+export default (filepath1, filepath2, format = 'stylish') => {
   const target = parser(filepath1);
   const source = parser(filepath2);
   return Formats[format](buildDiff(target, source));
